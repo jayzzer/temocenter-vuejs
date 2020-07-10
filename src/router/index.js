a1894d5d4eef7fe07203e3cc74374febb5c35508
jayzzer <pavel10_pushkarev@mail.ru>
2020-07-10 19:44:05 +0300

    Setup basic configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
 
 Vue.use(VueRouter);
 
@@ -9,14 +10,17 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      title: 'Главная',
+    },
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {
+      title: 'Вход',
+    },
   },
 ];
 
@@ -25,5 +29,10 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
+router.beforeEach = (to, _from, next) => {
+  document.title = to.meta.title;
+  next();
+};
 
 export default router;
